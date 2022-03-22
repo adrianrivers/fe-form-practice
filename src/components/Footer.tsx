a14691d9adb0bc4669405142915cf251c3223c16
Adrian Rivers <adrianlrivers@gmail.com>
2022-03-23 00:57:36 +1300

    :recycle: Add link to repo

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <Box as="footer" paddingY={8}>
       <Container maxWidth="container.sm">
-        <Link href="/" target="_blank">
+        <Link href="https://github.com/adrianrivers/fe-form-practice" target="_blank">
           Link to repository
         </Link>
       </Container>
