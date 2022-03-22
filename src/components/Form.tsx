a11bfa93d82d2d4e96f2836e91dae2fa3a51eb15
Adrian Rivers <adrianlrivers@gmail.com>
2022-03-22 17:23:16 +1300

    :sparkles: Complete and validate base form

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -2,72 +2,160 @@ import {
   Box,
   Button,
   chakra,
+  Checkbox,
+  CheckboxGroup,
   Container,
   FormControl,
-  // FormErrorMessage,
-  FormHelperText,
+  FormErrorMessage,
   FormLabel,
   HStack,
   Input,
   Radio,
   RadioGroup,
-  Select,
   Textarea,
 } from '@chakra-ui/react'
+import { FormikHelpers, useFormik } from 'formik'
+import React from 'react'
+import * as Yup from 'yup'
 
 const COLOURS = ['Blue', 'Green', 'Red', 'Black', 'Brown']
 const ANIMALS = ['Bear', 'Tiger', 'Snake', 'Donkey']
 
+interface FormValues {
+  email: string
+  password: string
+  colour: string
+  animal: string[]
+  tigerText: string
+}
+
+const INITIAL_VALUES: FormValues = {
+  email: '',
+  password: '',
+  colour: '',
+  animal: [],
+  tigerText: '',
+}
+
+const VALIDATION_SCHEMA = Yup.object({
+  email: Yup.string().email('Must be a valid email').max(255).required('Email is required'),
+  password: Yup.string()
+    .min(8, 'Password is too short - it should contain at least 8 letters')
+    .required('No password provided'),
+  colour: Yup.string().required('You must select a colour'),
+  animal: Yup.array().test({
+    name: 'animal_test',
+    exclusive: true,
+    message: 'You must select at least one animal',
+    test: (value = ['']) => value.length > 0,
+  }),
+  tigerText: Yup.string(),
+})
+
 export default function Form() {
+  const handleFormSubmit = async (values: FormValues, actions: FormikHelpers<FormValues>) => {
+    await new Promise((r) => setTimeout(r, 500))
+    const formattedValues = JSON.stringify(values, null, 2)
+
+    actions.setSubmitting(false)
+    alert(`Form submitted with values: ${formattedValues}`)
+    actions.resetForm()
+  }
+
+  const formik = useFormik({
+    initialValues: INITIAL_VALUES,
+    validationSchema: VALIDATION_SCHEMA,
+    onSubmit: handleFormSubmit,
+  })
+
   return (
     <Container maxWidth="container.sm">
-      <chakra.form
-        __css={{
-          '> *': {
-            marginBottom: 8,
-          },
-        }}
-      >
-        <FormControl>
-          <FormLabel as="legend">Email address</FormLabel>
-          <Input id="email" type="email" variant="brand" />
-          <FormHelperText>Please enter your email address.</FormHelperText>
+      <chakra.form __css={{ '> *': { marginBottom: 12 } }} onSubmit={formik.handleSubmit}>
+        <FormControl isInvalid={Boolean(formik.errors.email)}>
+          <FormLabel as="legend" htmlFor="email">
+            Email:
+          </FormLabel>
+          <Input
+            variant="brand"
+            type="email"
+            name="email"
+            placeholder="joebloggs@email.com"
+            onChange={formik.handleChange}
+            value={formik.values.email}
+          />
+          <FormErrorMessage>{formik.errors.email}</FormErrorMessage>
         </FormControl>
 
-        <FormControl>
-          <FormLabel as="legend">Password</FormLabel>
-          <Input id="email" type="password" variant="brand" />
-          <FormHelperText>Please enter a password.</FormHelperText>
+        <FormControl isInvalid={!!formik.errors.password && !!formik.touched.password}>
+          <FormLabel as="legend" htmlFor="password">
+            Password:
+          </FormLabel>
+          <Input
+            variant="brand"
+            type="password"
+            name="password"
+            onChange={formik.handleChange}
+            value={formik.values.password}
+          />
+          <FormErrorMessage>{formik.errors.password}</FormErrorMessage>
         </FormControl>
 
-        <FormControl as="fieldset">
-          <RadioGroup defaultValue="Blue">
+        <FormControl as="fieldset" id="colour" isInvalid={!!formik.errors.colour}>
+          <FormLabel as="legend" htmlFor="colour">
+            Choose a favourite colour:
+          </FormLabel>
+          <RadioGroup name="colour">
             <HStack spacing="24px">
               {COLOURS.map((choice, index) => (
-                <Radio key={`${index}-${choice}`} value={choice} colorScheme="green">
+                <Radio key={`${index}-${choice}`} colorScheme="green" value={choice} onChange={formik.handleChange}>
                   <Box color="brand.darkGray">{choice}</Box>
                 </Radio>
               ))}
             </HStack>
           </RadioGroup>
-          <FormHelperText>Please select a colour.</FormHelperText>
+          <FormErrorMessage>{formik.errors.colour}</FormErrorMessage>
         </FormControl>
 
-        <FormControl>
-          <FormLabel htmlFor="animal">Country</FormLabel>
-          <Select id="animal" placeholder="Select an animal" variant="brand">
-            {ANIMALS.map((choice, index) => (
-              <option key={`${index}-${choice}`}>{choice}</option>
-            ))}
-          </Select>
+        <FormControl as="fieldset" id="animal" isInvalid={!!formik.errors.animal && !!formik.errors.animal}>
+          <FormLabel as="legend" htmlFor="animal">
+            Select one or more favourite animals:
+          </FormLabel>
+          <CheckboxGroup>
+            <HStack spacing="24px">
+              {ANIMALS.map((choice, index) => (
+                <Checkbox
+                  key={`${index}-${choice}`}
+                  color="brand.darkGray"
+                  name="animal"
+                  value={choice}
+                  onChange={formik.handleChange}
+                  colorScheme="green"
+                >
+                  {choice}
+                </Checkbox>
+              ))}
+            </HStack>
+          </CheckboxGroup>
+          <FormErrorMessage>{formik.errors.animal}</FormErrorMessage>
         </FormControl>
 
-        <FormControl>
-          <FormLabel htmlFor="animal">Country</FormLabel>
-          <Textarea id="tiger-query" placeholder="Tigers want to eat you..." variant="brand"></Textarea>
-        </FormControl>
+        {formik.values.animal.includes('Tiger') && (
+          <FormControl>
+            <FormLabel as="legend" htmlFor="tigerText">
+              If you want, please tell us a story about a tiger:
+            </FormLabel>
+            <Textarea
+              id="tigerText"
+              placeholder="Once a 🐯 tried to eat me..."
+              variant="brand"
+              height="150px"
+              onChange={formik.handleChange}
+              value={formik.values.tigerText}
+            />
+          </FormControl>
+        )}
 
-        <Button colorScheme="green" color="brand.darkGray" _hover={{ backgroundColor: '#0da053' }}>
+        <Button colorScheme="green" color="brand.darkGray" _hover={{ backgroundColor: '#0da053' }} type="submit">
           Submit
         </Button>
       </chakra.form>
